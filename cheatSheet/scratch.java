e0b0680194dbc6fbd96c3a065ab625f57b5789ce
evanengelen <evanengelen@bol.com>
2021-01-03 07:30:40 +0100

    add cheat sheet

---
+++ cheatSheet/scratch.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class Scratch {
+    public static void main(String[] args) {
+        
+    }
+}
