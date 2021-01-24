821a075fc403bc9bcbc2943c7f87b0eafe7cb32f
evanengelen <evanengelen@bol.com>
2021-01-24 08:59:21 +0100

    update test

--- src/test/java/nl/vintik/example/java/junit5/parametarized/FeatureTest.java
+++ src/test/java/nl/vintik/example/java/junit5/parametarized/FeatureTest.java
@@ -2,12 +2,7 @@ package nl.vintik.example.java.junit5.parametarized;
 
 import nl.vintik.example.java.junit5.mockito.Toggle;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.Arrays;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -16,11 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class FeatureTest {
 
-    public static Stream<Arguments> data() {
-        return Arrays.stream(Feature.values()).map(Arguments::of);
-    }
-
-    @ParameterizedTest
+    @ParameterizedTest(name = "#{index} - value of {0} should be between 5 and 35")
     @EnumSource(Feature.class)
     public void shouldContainKeysWithLengthBetween5And35Inclusive(Toggle toggle) {
         assertTrue(toggle.name().length() >= 5);
